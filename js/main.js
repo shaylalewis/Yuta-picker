2bdf1fdeefdc8d91918a8b45b3975c8fb938eb75
crime boy <62856686+shaylalewis@users.noreply.github.com>
2022-02-27 17:32:10 -0500

    Moved files
    put files in correct folder

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.getElementById('yutaOne').onclick = nayuOne
+document.getElementById('yutaTwo').onclick = nayuTwo
+document.getElementById('yutaThree').onclick = nayuThree
+document.getElementById('yutaFour').onclick = nayuFour
+document.getElementById('yutaFive').onclick = nayuFive
+
+
+
+function nayuOne() {
+  var audio = document.getElementById("audioOne");
+  audio.play();
+}
+
+function nayuTwo() {
+  var audio = document.getElementById("audioTwo");
+  audio.play();
+}
+
+function nayuThree() {
+  var audio = document.getElementById("audioThree");
+  audio.play();
+}
+
+function nayuFour() {
+  var audio = document.getElementById("audioFour");
+  audio.play();
+}
+
+function nayuFive() {
+  var audio = document.getElementById("audioFive");
+  audio.play();
+}
